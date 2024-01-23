e874b80010c77ffe9346f00919a9b3e5f86e58f3
kaditya125 <80103410+kaditya125@users.noreply.github.com>
2024-01-23 19:38:31 +0530

    commited

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [counter, setCounter] = useState(15);
+
+  const addValue = () => {
+    if (counter < 50) {
+      setCounter((prevCounter) => prevCounter + 1);
+    }
+  };
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter((prevCounter) => prevCounter - 1);
+    }
+  };
+
+  return (
+    <>
+      <h1>Ashu's RSS wing</h1>
+      <h2>Rss member's counter: {counter}</h2>
+
+      <button onClick={addValue}>Add members</button>
+      <button onClick={removeValue}>Remove Members</button>
+    </>
+  );
+}
+
+export default App;
